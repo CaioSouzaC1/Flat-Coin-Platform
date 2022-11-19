300193f36da87ae7f85254a2977e86b65c416bf6
Caio César de Souza <108900609+CaioSouzaC1@users.noreply.github.com>
2022-11-19 13:16:44 -0300

    Initializing index

--- index.php
+++ index.php
@@ -9,13 +9,29 @@
     <link rel="stylesheet" href="./src/css/ResetCss.css">
     <link rel="stylesheet" href="./src/css/Main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;300;400;600;700;900&display=swap" rel="stylesheet">
     <title>FlatCoin Platform</title>
 </head>
 
 <body>
     <header>
-        <div class="row main_container"><i id="logo" class="fa-brands fa-bitcoin"></i></div>
+        <div class="row main_container jc-sb ai-ce">
+            <div class="row jc-sb ai-ce logos">
+                <i id="logo" class="fa-brands fa-bitcoin"></i>
+                <span>FlatCoin</span>
+                <span>Platform</span>
+            </div>
+            <div class="menu row pointer">
+                <ul>
+                    <li>Top Cryptos</li>
+                    <li>Top Cryptos</li>
+                    <li>Top Cryptos</li>
+                </ul>
+                <i id="hamburguer" class="fa-solid fa-bars"></i>
+            </div>
+        </div>
     </header>
 </body>
 
--- src/js/Script.js
+++ src/js/Script.js
@@ -1,13 +1,40 @@
 const logo = document.querySelector("#logo");
-const func1 = () => {
+
+const changeLogo = () => {
   setInterval(() => {
     if (logo.classList == "fa-brands fa-bitcoin") {
+      document.querySelector(".logos span").style.color = "#051338";
+      document.querySelector(".logos span:nth-child(3)").style.color =
+        "#f7931a";
       logo.setAttribute("class", "fa-brands fa-ethereum");
     } else {
+      document.querySelector(".logos span").style.color = "#f7931a";
+      document.querySelector(".logos span:nth-child(3)").style.color =
+        "#051338";
       logo.setAttribute("class", "fa-brands fa-bitcoin");
     }
-  }, 5000);
+  }, 4500);
 };
 window.addEventListener("load", () => {
-  func1();
+  changeLogo();
+});
+
+const maybeShowHam = () => {
+  if (hamburguer.classList == "fa-solid fa-bars") {
+    hamburguer.setAttribute("class", "fa-solid fa-xmark");
+    document.querySelector(".menu ul").classList.add("active");
+  } else {
+    document.querySelector(".menu ul").classList.remove("active");
+    hamburguer.setAttribute("class", "fa-solid fa-bars");
+  }
+};
+
+const hamburguer = document.querySelector("#hamburguer");
+hamburguer.addEventListener("click", () => {
+  maybeShowHam();
+});
+Array.from(document.querySelectorAll(".menu ul li")).map((e) => {
+  e.addEventListener("click", () => {
+    maybeShowHam();
+  });
 });
