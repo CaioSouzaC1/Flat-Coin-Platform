5a316e8cc77cb455ef2f2509e765f97ea1431f0b
Caio César de Souza <108900609+CaioSouzaC1@users.noreply.github.com>
2022-11-18 21:32:11 -0300

    Making CryptoQuotes Function and improve ListingAllCryptos

--- ListingAllCryptos.php
+++ ListingAllCryptos.php
@@ -1,10 +1,13 @@
 <?php
+
+require "CryptoQuotes.php";
+
 //https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyMap
 //List all cryptos
 $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map';
 $parameters = [
     'start' => '1',
-    'limit' => '30',
+    'limit' => '5',
     'sort' => 'cmc_rank'
 ];
 
@@ -25,7 +28,38 @@ curl_setopt_array($curl, array(
 ));
 
 $response = curl_exec($curl); // Send the request, save the response
-echo ("<pre>");
-print_r(json_decode($response)); // print json decoded response
-echo ("</pre>");
 curl_close($curl); // Close request
+
+$cryptos = json_decode($response);
+
+$topCryptoIds = '';
+foreach ($cryptos->data as $cry) {
+    $topCryptoIds = $topCryptoIds . $cry->id . ',';
+}
+$topCryptoIds = rtrim($topCryptoIds, ",");
+
+$convertCoin = 'BRL';
+
+$deepCryptoInfo = listDeepInfoCrypto($topCryptoIds, $convertCoin, $headers);
+
+echo ("<pre>");
+print_r($deepCryptoInfo);
+echo ("</pre>");
+
+//rand(0,9)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+</body>
+
+</html>
