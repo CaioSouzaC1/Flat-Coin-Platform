677eec81268ff48ed9781a0415cae2933fc633ef
Caio César de Souza <108900609+CaioSouzaC1@users.noreply.github.com>
2022-11-24 23:25:06 -0300

    continuing js

--- index.php
+++ index.php
@@ -74,39 +74,7 @@ require "header.php";
     </div>
     <div id="teste"></div>
 
-    <ul>
-        <?php
-        foreach ($cryptos[0] as $SingleCrypto) {
-        ?>
-            <li class="Crypto" onclick='openModal(<?= json_encode($SingleCrypto) ?>)'>
-                <div class="row ai-ce">
-                    <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 row ai-ce infos">
-                        <div class="row ai-ce col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
-                            <img src="<?= $SingleCrypto[8] ?>" alt="<?= $SingleCrypto[1] ?>">
-                            <h1 class="CryptoName"><?= $SingleCrypto[1] ?></h1>
-                        </div>
-                        <div class="row ai-ce col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                            <h5>Abrev:</h5>
-                            <h6><?= $SingleCrypto[2] ?></h6>
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 row ai-ce">
-                        <a href="<?= $SingleCrypto[9] ?>"><i class="fa-solid fa-globe"></i></a>
-                        <i class="fa-solid fa-hand-holding-dollar"></i> <?= $SingleCrypto[4][0] ?>
-                        % last 24%
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-graph-down-arrow text-red" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0Zm10 11.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 0-1 0v2.6l-3.613-4.417a.5.5 0 0 0-.74-.037L7.06 8.233 3.404 3.206a.5.5 0 0 0-.808.588l4 5.5a.5.5 0 0 0 .758.06l2.609-2.61L13.445 11H10.5a.5.5 0 0 0-.5.5Z" />
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-graph-up-arrow text-green" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0Zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5Z" />
-                        </svg>
-                        <?= $SingleCrypto[4][4] ?>
-                    </div>
-
-                </div>
-            </li>
-        <?php }
-        ?>
+    <ul id="ulMain">
     </ul>
 </main>
 <section id="newsletter">
--- src/js/Script.js
+++ src/js/Script.js
@@ -19,6 +19,7 @@ const changeLogo = () => {
 window.addEventListener("load", () => {
   changeLogo();
   coinSelect();
+  loadJSON("coin=USD");
 });
 
 const maybeShowHam = () => {
@@ -75,9 +76,7 @@ let $testeDiv = document.querySelector("#teste");
 
 function loadJSON(params) {
   document.querySelector("#loader").classList.remove("invisible");
-  let myObj,
-    x,
-    htmlResult = "";
+  let myObj = "";
   let url = "FetchPostApi.php";
   fetch(url, {
     method: "POST",
@@ -93,10 +92,45 @@ function loadJSON(params) {
     .then(function (myJson) {
       myObj = JSON.parse(JSON.stringify(myJson));
       console.log(myObj);
-      for (x in myObj) {
-        htmlResult += myObj[x].Price + "</b><br>";
-      }
+      myObj.forEach((element) => {
+        document.querySelector("#ulMain").innerHTML += `
+        <li class="Crypto" onclick='openModal(${element})'>
+        <div class="row ai-ce">
+            <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 row ai-ce infos">
+                <div class="row ai-ce col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+                    <img src="${element.Logo}" alt="${element.CryptoName}">
+                    <h1 class="CryptoName">${element.CryptoName}</h1>
+                </div>
+                <div class="row ai-ce col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                    <h5>Abrev:</h5>
+                    <h6>${element.Abrev}</h6>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 row ai-ce">
+                <a href="${element.Site}"><i class="fa-solid fa-globe"></i></a>
+                <i class="fa-solid fa-hand-holding-dollar"></i> ${eval(
+                  element.Price
+                ).toFixed(5)}
+                % last 24%
+                ${eval(element.h1).toFixed(5)}
+                ${verifyPercentChange(eval(element.h1))}  
+            </div>
+        </div>
+    </li>
+        `;
+      });
       document.querySelector("#loader").classList.add("invisible");
-      $testeDiv.innerHTML = htmlResult;
     });
 }
+
+const verifyPercentChange = (valor) => {
+  if (valor > 0) {
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-graph-up-arrow text-green" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0Zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5Z" />
+  </svg>`;
+  } else {
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-graph-down-arrow text-red" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0Zm10 11.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 0-1 0v2.6l-3.613-4.417a.5.5 0 0 0-.74-.037L7.06 8.233 3.404 3.206a.5.5 0 0 0-.808.588l4 5.5a.5.5 0 0 0 .758.06l2.609-2.61L13.445 11H10.5a.5.5 0 0 0-.5.5Z" />
+  </svg>`;
+  }
+};
